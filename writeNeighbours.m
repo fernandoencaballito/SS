e041c4fe3cac193a6f1e7a29937fa33e7d23decf
Luis Marzoratti <lmarzora@itba.edu.ar>
2016-03-14 17:35:30 -0300

    fix delimiter and particle id

--- writeNeighbours.m
+++ writeNeighbours.m
@@ -2,6 +2,7 @@ function writeNeighbours(outputFile,neighbours,N)
 	fid=fopen(outputFile,'w');
 	fclose(fid);
 	for k = 1:N
-			dlmwrite(outputFile,neighbours{1,k},"-append","newline","unix");
+		output = [k,neighbours{1,k}];
+			dlmwrite(outputFile,output," ","-append","newline","unix");
 	endfor
 endfunction
